5c550a265c5a48155ad5eb616925382c85405bfc
vitorlindo07 <victorhsilva024@gmail.com>
2024-12-03 19:22:41 -0300

    Final

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é JavaScript',
+    'O JavaScript é uma linguagem de programação'
+)
+
+criaCartao (
+    'Geografia',
+    'Qual a capital da França',
+    'A capital da França é Paris'
+)
+
+criaCartao (
+    'programação',
+    'O que é uma função',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao (
+    'Idioma',
+    'Como se diz oi em inglês',
+    'Oi em inglês se diz HI (RAI)'
+)
